e2e9b79b82f671b5f237c1cf33f1822fb3ecfa7c
Cassandre KANT <60017206+casskant@users.noreply.github.com>
2026-02-11 12:01:19 +0100

    Fix logical operator for CORS origin

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.use(cors({
-  origin: process.env.ORIGIN | "http://localhost:3000",
+  origin: process.env.ORIGIN || "http://localhost:3000",
   credentials: true
 }));
 
